45f37639618fdec02ef0f35aa350e1b98a98b949
Trevor <xizhouh@gmail.com>
2018-02-13 02:49:39 -0600

    fix: Fix vTitle

--- app/web/component/layout/index.js
+++ app/web/component/layout/index.js
@@ -4,7 +4,7 @@ const content = `<div id="app">
   <main-layout>
     <div slot="banner">
       <slot name="banner-title">
-        <h1 slot="banner-title">{{vTitle}}</h1>
+        <h1 slot="banner-title">{{currentUrl}}</h1>
         <small slot="banner-title">{{secTitle}}</small>
       </slot>
     </div>
@@ -55,9 +55,6 @@ export default {
   },
 
   computed: {
-    vTitle() {
-      return this.$root.title || this.title;
-    },
     vKeywords() {
       return this.$root.keywords || this.keywords;
     },
@@ -66,6 +63,38 @@ export default {
     },
     vSecTitle() {
       return this.$root.secTitle || this.secTitle;
+    },
+    vTitle() {
+      if (this.$route.path === '/') {
+        return 'TeanBlog';
+      }
+      if (this.$route.path === '/blog') {
+        return '博客';
+      }
+      if (this.$route.path === '/archive') {
+        return '归档';
+      }
+      if (this.$route.path === '/about') {
+        return '关于';
+      }
+      return 'TeanBlog';
+    }
+  },
+
+  methods: {
+    getUrlRelativePath() {
+      if (process.browser) {
+        const url = document.location.toString();
+        const arrUrl = url.split('//');
+        const start = arrUrl[1].indexOf('/');
+        let relUrl = arrUrl[1].substring(start);
+
+        if (relUrl.indexOf('?') !== -1) {
+          relUrl = relUrl.split('?')[0];
+        }
+
+        return relUrl;
+      }
     }
   },
 
